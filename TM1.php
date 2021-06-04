87d355344decc6f928951b06b43ea902c0c51278
Atarpoenya <71941828+atarpoeanya@users.noreply.github.com>
2021-06-05 02:34:41 +0900

    Create TM1.php

---
+++ TM1.php
@@ -0,0 +1,34 @@
+
+
+
+
+<?php
+
+class Runner {
+
+}
+
+interface MachineInput {
+    //menerima input
+    //Membuat tape
+    function konvertKeTape($a, $b);
+
+}
+
+
+class Machine implements MachineInput{
+
+    public function konvertKeTape($a, $b)
+    {
+           
+    }
+
+}
+
+
+
+
+
+
+
+?>
